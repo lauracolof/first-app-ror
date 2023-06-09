e47e109ec43fc936b31dee02491cec17b36d1d5b
Laura <lauracolof@gmail.com>
2023-06-09 11:33:45 -0300

    change node v and create about-us

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # http://127.0.0.1:3000/
   root 'pages#home'
+
   get 'about' => 'pages#about'
+  get 'contact-us' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
