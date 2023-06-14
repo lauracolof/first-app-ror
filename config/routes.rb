0ab58aa99a7240ad5abd6c7fe5cf0e086f5ff68b
Laura <lauracolof@gmail.com>
2023-06-14 18:16:58 -0300

    routes to: /log_out, /sign_in && /sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
-  get 'contact-us' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
