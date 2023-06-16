f238ad8fce678e51fd3af58d8e8f97b5082639bc
Laura <lauracolof@gmail.com>
2023-06-16 18:12:51 -0300

    creating an admin for the first time with rails console

--- app/models/admin_authorization.rb
+++ app/models/admin_authorization.rb
@@ -5,3 +5,15 @@ class AdminAuthorization < ActiveAdmin::AuthorizationAdapter
     user && user.admin?
   end
 end
+
+#! UNIQUE WAY TO CREATE A ADMIN FOR THE FIRST TIME => rails console
+# user = User.find_by(email: "example@mail.com")
+# user.email => example@mail.com
+# user.id => 1
+# user.created_at => Wed, 14 Jun 2023 21:15:12.274983000 UTC +00:00
+# user.admin => false 
+# user.admin = true
+# user => #<User id: 1, email: "example@mail.com", created_at: "2023-06-14 21:15:12.274983000 +0000", updated_at: "2023-06-14 21:15:12.274983000 +0000", admin: true>
+# user.save =>   TRANSACTION (0.1ms)  begin transaction
+  # User Update (13.6ms)  UPDATE "users" SET "updated_at" = ?, "admin" = ? WHERE "users"."id" = ?  [["updated_at", "2023-06-16 21:09:28.842872"], ["admin", 1], ["id", 1]]
+  # TRANSACTION (3.0ms)  commit transaction
--- db/migrate/20230616201832_add_admin_to_users.rb
+++ db/migrate/20230616201832_add_admin_to_users.rb
@@ -3,3 +3,4 @@ class AddAdminToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :admin, :boolean, default: false
   end
 end
+
