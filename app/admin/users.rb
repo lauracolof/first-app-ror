0f302900b01de9c35e87a2c15f705fa8f9a9b036
Laura <lauracolof@gmail.com>
2023-06-29 23:09:52 -0300

    adding to users, name and username

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -15,11 +15,13 @@ ActiveAdmin.register User do
   #   permitted
   # end
 
-  permit_params :email, :admin
+  permit_params :email, :admin, :name, :username
 
   form do |f|
     f.semantic_errors
     f.inputs do
+      f.input :name 
+      f.input :username
       f.input :email
       f.input :admin
     end
@@ -28,6 +30,8 @@ ActiveAdmin.register User do
   
   index do
     id_column
+    column :name 
+    column :username
     column :email
     column :admin
     actions
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def access_denied(exception)
     # redirigir al root with an alert if the user try access to a place they cant
     redirect_to root_path, alert: exception.message
   end
 
+  protected 
+
+  def configure_permitted_parameters
+    devise_parameters_sanitizer.permite(:sign_up, keys: [:username, :name])
+  end
 end
