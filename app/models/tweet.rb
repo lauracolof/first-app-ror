c18d44b577718b8d0b7f169b105522a220286099
Laura <lauracolof@gmail.com>
2023-06-29 22:43:50 -0300

    add validations to tweets and items

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,9 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  # rails validations
+  # si el del current_user y existe tweet
+  validates :user, presence: true
+  # si existe content
+  validates :content, presence: true, length: { maximum: 280 }
 end
